f12a5cb3d23df1f58604e4ccf06c55b53c3fa719
Jashkumar Solanki <jashkumarsolanki@gmail.com>
2024-06-13 11:53:47 +0530

    Added Base Url

--- app/src/main/java/com/example/themovieapp/model/Result.java
+++ app/src/main/java/com/example/themovieapp/model/Result.java
@@ -2,15 +2,25 @@ package com.example.themovieapp.model;
 
 import android.graphics.Movie;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public class Result {
 
 
-
+    @SerializedName("page")
+    @Expose
     private Integer page;
+    @SerializedName("total_pages")
+    @Expose
     private Integer totalPage;
+    @SerializedName("total_results")
+    @Expose
     private Integer totalResult;
+    @SerializedName("results")
+    @Expose
     private List<Movie> results = null;
 
     public Integer getPage() {
